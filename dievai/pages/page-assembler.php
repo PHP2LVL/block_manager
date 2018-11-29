80ff14dcd769c042ddf004f4743adbd1c9557ea5
jonasburneika <jonas.burneika@gmail.com>
2018-11-29 21:09:25 +0200

    file relocation and renaming

--- dievai/pages/page-assembler.php
+++ dievai/pages/page-assembler.php
@@ -1,3 +1,9 @@
-<?php
-var_dump($_seesions);
-echo "test";
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-2">
+            <h4> Blokai</h4>
+        </div>
+        <div class="col-lg-10"></div>
+    </div>
+</div>
