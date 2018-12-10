652fa0666ee20c2a0c414ac59e5e3f0af974b236
TadasP <TadasPutjato@gmail.com>
2018-12-10 20:22:32 +0200

    Sutrumpintas Block Adding Menu

--- dievai/pages/page-assembler.php
+++ dievai/pages/page-assembler.php
@@ -28,13 +28,19 @@ if (isset($url['c'])) {
         $formClass = new Form($settings);
         lentele($lang['admin']['pageassembler_add'], $formClass->form());
         ?>
-        <div id="page-builder-zone">
-            <div class="row holder"  id='2'> <!-- PARENT BLOCK--> 
-                <div class="col-lg-12 crop" onclick="addClassBox(this)" id='666'>
-                    <img src="https://motionarray-portfolio.imgix.net/preview-83721-73b2ffab8d22cad99c5c66f9b51b4993-high.jpg" class="img-fluid max-width"  alt="Snow">
-                    <H1 class="top-left">Welcome to Page Assembler</h1>
-                    <span class="left-centered">Mauris dui enim, commodo at hendrerit a, pulvinar ut felis. Aliquam eu est ut nisi tincidunt facilisis. Phasellus porttitor vehicula eros, eget fermentum ex consequat vel. Nam fermentum, tortor quis congue maximus, magna dui mollis enim, vel efficitur ligula magna in urna. In interdum ipsum sit amet commodo lobortis. Aliquam erat volutpat. Morbi vitae nisi quis urna semper bibendum. Aenean hendrerit vel mi sit amet fringilla. Nunc convallis dui sed ultrices rhoncus. Vestibulum ac pulvinar erat. Proin dignissim ultricies metus eu luctus. Vivamus in bibendum quam. Aenean non fermentum nisi, a ultrices nisl. Sed sit amet tincidunt est. Nulla sit amet nibh turpis. Curabitur a finibus enim, a volutpat justo.</span>
-                    <button type="submit" class ="bottom-right">BUTTON</button>
+            <div id="page-builder-zone">
+                <!-- 
+                    ZONA BLOKŲ DĖLIOJIMUI 
+                    IR PUSLAPIO KONSTRAVIMUI
+                -->
+                <!-- <div class="manu-image-area">img</div> -->
+                <div class="row">
+                    <div class="col-lg-12 crop">
+                        <img src="https://motionarray-portfolio.imgix.net/preview-83721-73b2ffab8d22cad99c5c66f9b51b4993-high.jpg" class="img-fluid max-width"  alt="Snow">
+                        <H1 class="top-left">Welcome to Page Assembler</h1>
+                        <p class="left-centered">Mauris dui enim, commodo at hendrerit a, pulvinar ut felis. Aliquam eu est ut nisi tincidunt facilisis. Phasellus porttitor vehicula eros, eget fermentum ex consequat vel. Nam fermentum, tortor quis congue maximus, magna dui mollis enim, vel efficitur ligula magna in urna. In interdum ipsum sit amet commodo lobortis. Aliquam erat volutpat. Morbi vitae nisi quis urna semper bibendum. Aenean hendrerit vel mi sit amet fringilla. Nunc convallis dui sed ultrices rhoncus. Vestibulum ac pulvinar erat. Proin dignissim ultricies metus eu luctus. Vivamus in bibendum quam. Aenean non fermentum nisi, a ultrices nisl. Sed sit amet tincidunt est. Nulla sit amet nibh turpis. Curabitur a finibus enim, a volutpat justo.</p>
+                        <button type="submit" class ="bottom-right">BUTTON</button>
+                    </div>
                 </div>
             </div>
             <div class="row d-flex holder" id='1'> <!-- PARENT BLOCK-->
@@ -79,52 +85,21 @@ if (isset($url['c'])) {
             <div class="dropdown col-lg-12">
                 <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Add New Block
                 <span class="caret"></span></button>
+                <?php $categoriesDir = 'config/blocks/'; ?>
+                <?php $categories = array_diff(scandir($categoriesDir), array('..', '.')); ?>
                 <ul class="dropdown-menu main-dropdown">
-                    <li class="dropdown-submenu">
-                        <a class="test" tabindex="-1" href="#">Text Blocks<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a tabindex="-1" class="add-block test" data-href="config/blocks/text_blocks/title-block.html">Title Block </a></li>
-                            <li><a tabindex="-1" class="add-block test" data-href="config/blocks/text_blocks/heading-block.html">Heading Block</a></li>
-                            <li><a tabindex="-1" class="add-block test" data-href="config/blocks/text_blocks/paragraph-block.html">Paragraph Block</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown-submenu">
-                        <a class="test" tabindex="-1" href="#">Image Blocks<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a tabindex="-1" class="add-block test"  data-href="config/blocks/image_blocks/image-block.html">Image Block</a></li>
-                            <li><a tabindex="-1" class="add-block test"  data-href="config/blocks/image_blocks/image-description-block.html">Image with Description Block</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown-submenu">
-                        <a class="test" tabindex="-1" href="#">Button Blocks<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a tabindex="-1" class="add-block test" data-href="config/blocks/button_blocks/default-button-block.html">Default Button Block</a></li>
-                            <li><a tabindex="-1" class="add-block test" data-href="config/blocks/button_blocks/danger-button-block.html">Danger Button Block</a></li>
-                            <li><a tabindex="-1" class="add-block test" data-href="config/blocks/button_blocks/button-group-block.html">Button Group Block</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown-submenu">
-                        <a class="test" tabindex="-1" href="#">Gallery Blocks<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a tabindex="-1" class="add-block test" data-href="config/blocks/gallery_blocks/simple-gallery-block.html">Simple Gallery Block</a></li>
-                            <li><a tabindex="-1" class="add-block test" data-href="config/blocks/gallery_blocks/carousel-gallery-block.html">Carousel Gallery Block</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown-submenu">
-                        <a class="test" tabindex="-1" href="#">List Blocks<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a tabindex="-1" class="add-block test" data-href="config/blocks/list_blocks/simple-list-block.html">Simple List Block</a></li>
-                            <li><a tabindex="-1" class="add-block test" data-href="config/blocks/list_blocks/menu-list-block.html">Menu List Block</a></li>
-                            <li><a tabindex="-1" class="add-block test" data-href="config/blocks/list_blocks/table-block.html">Table Block</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown-submenu">
-                        <a class="test" tabindex="-1" href="#">Form Blocks<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a tabindex="-1" class="add-block test" data-href="config/blocks/form_blocks/registration-form-block.html">Registration Form Block</a></li>
-                            <li><a tabindex="-1" class="add-block test" data-href="config/blocks/form_blocks/feedback-form-block.html">Feedback Form Block</a></li>
-                        </ul>
-                    </li>
+                    <?php foreach ($categories as $category): ?>
+                        <?php echo '<li class="dropdown-submenu">'?>
+                            <?php echo '<a class="test" tabindex="-1" href="#">'.$category.'<span class="caret"></span></a>'?>
+                            <?php echo '<ul class="dropdown-menu">' ?>
+                                <?php $blocksDir = 'config/blocks/'.$category; ?>
+                                <?php $blocks = array_diff(scandir($blocksDir), array('..', '.')); ?>
+                                <?php foreach ($blocks as $block): ?>
+                                <?php echo '<li><a tabindex="-1" class="add-block test" data-href="config/blocks/'.$category.'/'.$block.'">'.substr($block,0,-5).'</a></li>' ?>
+                                <?php endforeach; ?>
+                            <?php echo '</ul>'; ?>
+                        <?php echo '</li>' ?>
+                    <?php endforeach; ?>
                 </ul>
             
             <!-- 
@@ -151,6 +126,8 @@ if (isset($url['c'])) {
         </div>
         
         <script type="text/javascript" src="js/page-assembler.js"></script>
+            </div>
+        </div>
     <?php }  
     if ($url['c'] == 'list') {
         $settings = [ 
